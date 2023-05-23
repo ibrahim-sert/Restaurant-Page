60004713ef9077dbf7f7d3d3ed001c628490e504
ibrahim-sert <evo3506@gmail.com>
2023-05-23 15:01:36 +0300

    Restaurant menu started

---
+++ src/App.js
@@ -0,0 +1,107 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div class="orbit">
+      <ul class="orbit-wrap">
+        <li className="orbit-center ">
+          <img
+            className="rounded-full"
+            src="https://d17wu0fn6x6rgz.cloudfront.net/img/w/tarif/mgt/ykvmayiswebsite26.webp"
+            alt=""
+          />
+          {/* <i class="orbit-center__icon fa fa-code"></i> */}
+        </li>
+
+        <li>
+          <ul class="ring-0">
+            <li>
+              <i class="orbit-icon fa fa-git"></i>
+            </li>
+            <li>
+              <i class="orbit-icon fa fa-free-code-camp"></i>
+            </li>
+            <li>
+              <i class="orbit-icon fa fa-meetup"></i>
+            </li>
+            <li>
+              <i class="orbit-icon fa fa-codepen"></i>
+            </li>
+          </ul>
+        </li>
+
+        {/* <li>
+          <ul class="ring-1">
+            <li>
+              <i class="orbit-icon fa fa-podcast"></i>
+            </li>
+            <li>
+              <i class="orbit-icon fa fa-css3"></i>
+            </li>
+            <li>
+              <i class="orbit-icon fa fa-html5"></i>
+            </li>
+          </ul>
+        </li> */}
+        {/* <li>
+          <ul class="ring-2">
+            <li>
+              <i class="orbit-icon fa fa-windows"></i>
+            </li>
+            <li>
+              <i class="orbit-icon fa fa-safari"></i>
+            </li>
+            <li>
+              <i class="orbit-icon fa fa-edge"></i>
+            </li>
+            <li>
+              <i class="orbit-icon fa fa-linux"></i>
+            </li>
+            <li>
+              <i class="orbit-icon fa fa-apple"></i>
+            </li>
+            <li>
+              <i class="orbit-icon fa fa-chrome"></i>
+            </li>
+            <li>
+              <i class="orbit-icon fa fa-android"></i>
+            </li>
+            <li>
+              <i class="orbit-icon fa fa-firefox"></i>
+            </li>
+          </ul>
+        </li> */}
+        {/* <li>
+          <ul class="ring-3">
+            <li>
+              <i class="orbit-icon fa fa-coffee"></i>
+            </li>
+            <li>
+              <i class="orbit-icon fa fa-terminal"></i>
+            </li>
+            <li>
+              <i class="orbit-icon fa fa-heart-o"></i>
+            </li>
+          </ul>
+        </li> */}
+      </ul>
+    </div>
+    // <div
+    // className="relative flex justify-center items-center h-screen w-screen "
+    // >
+
+    //   <div className="absolute w-5 h-5 rounded-full bg-black bottom-52 left-96"></div>
+    //   <img
+    //     className="h-[50vh] w-[100vh]
+    //   rounded-tl-full rounded-tr-full border-4 border-solid border-black p-6"
+    //     // className="rounded-full h-[100vh] w-[100vh] m-auto p-6"
+    //     src="https://cdn.yemek.com/mnresize/1250/833/uploads/2015/01/sucuklu-pizza-yeni-onecikan.jpg"
+    //     alt=""
+    //   />
+    // </div>
+  );
+};
+
+export default App;
+
+// text-amber-500 border-4 border-solid border-black rounded-full h-[100vh] w-[100vh] m-auto
